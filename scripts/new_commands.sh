9f58fdd02b7f619c0f96d5761d1f1e82b7245d62
Under_Dog <zhao_qing78@163.com>
2023-11-24 11:41:18 +0000

    add new command

--- scripts/new_commands.sh
+++ scripts/new_commands.sh
@@ -2,6 +2,18 @@
 # train : [syn_pretrain / pretrain / finetune / scratch]
 # batch image_size epoch lr
 
+# quad bayer baseline
+CUDA_VISIBLE_DEVICES='0,1,2,3,4,5,6,7' bash run_raw.sh train QuadRAW quadraw_unet_pares4_new quad_pretrain 'ep200,lr2e-4,charb' --charbonnier --lr 2e-4 --epoch 200
+# quad bayer baseline + gw
+CUDA_VISIBLE_DEVICES='0,1,2,3,4,5,6,7' bash run_raw.sh train QuadRAW quadraw_unet_pares4_new quad_pretrain 'ep200,lr2e-4,charb,gw3' --charbonnier --gw_loss_weight 3 --lr 2e-4 --epoch 200
+# quad bayer baseline + newfusion
+CUDA_VISIBLE_DEVICES='0,1,2,3,4,5,6,7' bash run_raw.sh train QuadRAW quadraw_newfusion_unet_pares4_new quad_pretrain 'ep200,lr2e-4,charb' --charbonnier --lr 2e-4 --epoch 200
+# quad bayer baseline + pretrain
+# (pretrain)
+CUDA_VISIBLE_DEVICES='0,1,2,3,4,5,6,7' bash run_raw.sh train QuadRAW quadraw_unet_pares4_new quad_syn_pretrain 'bs64,ps384,ep200,lr2e-4,charb' --charbonnier --lr 2e-4 --epoch 200 --image_size 384 --batch 64
+# (finetune) + gw
+CUDA_VISIBLE_DEVICES='0,1,2,3,4,5,6,7' bash run_raw.sh train QuadRAW quadraw_unet_pares4_new quad_pretrain 'syn(bs64,ps384,ep200,lr2e-4,charb),bs32,ps384,ep200,lr2e-4,charb,gw3' --charbonnier --gw_loss_weight 3 --lr 2e-4 --epoch 200 --image_size 384 --batch 32 --load_run_name 'QuadRAW-quadraw_unet_pares4_new-quad_syn_pretrain-(bs64,ps384,ep200,lr2e-4,charb)'
+
 CUDA_VISIBLE_DEVICES='0,1,2,3' bash run.sh train RGB unet_pares4_new syn_pretrain 'bs64,ps384,ep100,lr5e-4,charb' --charbonnier --lr 5e-4 --epoch 100 --image_size 384 --batch 64
 
 CUDA_VISIBLE_DEVICES='0,1,2,3' bash run.sh train RGB unet_pares4_new pretrain 'synPTsmall(bs32,ps384,ep100,lr2e-4,charb),ep100,lr1e-4,charb' --load_run_name 'RGB-unet_pares4_new-syn_pretrain_small-(bs32,ps384,ep100,lr2e-4,charb)' --charbonnier --lr 1e-4 --epoch 100
